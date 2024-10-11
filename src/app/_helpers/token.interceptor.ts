8677bb34a4ad2ea836c3d547c39caaf556d228bb
toonette88 <156116373+toonette88@users.noreply.github.com>
2024-10-11 18:01:10 +0200

    Tentative de récupération du token

--- src/app/_helpers/token.interceptor.ts
+++ src/app/_helpers/token.interceptor.ts
@@ -1,21 +1,26 @@
 import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { TokenService } from "../_services/token.service";
 import { inject } from "@angular/core";
-
-
+import { TokenService } from "../_services/token.service";
 
 export function tokenInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
-  // Inject the current `TokenService` and use it to get an authentication token:
-  const token = inject(TokenService).getToken();
-  // Clone the request to add the authentication header.
-  
-  if(token !== null ){
-    console.log(token)
-    const newReq = req.clone();
-    console.log(newReq)
-    return next(newReq);
-  }
+
+  //Injection du serviceToken et obtention du token
+const token = inject(TokenService).getToken();
+
+//Si token, clonage et  modification de la requête
+if(token){
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', token)
+});
+return next(newReq)
+} else {
 
   return next(req)
-  
 }
+
+
+
+
+}
+
+
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { tokenInterceptor } from './_helpers/token.interceptor';
+import { tokenInterceptorProvider } from './_helpers/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers:[
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(
-      withInterceptors([tokenInterceptor])
-    ),
-  ]
-};
+    provideHttpClient(),
+]};
